d0e35d091801521118ed047e24e0256ea3644781
Karina Naumova <karinka_naumova@mail.ru>
2022-10-18 18:19:21 +0300

    добавлена 8 задача

---
+++ seminar_1/consoleApp_8/Program.cs
@@ -0,0 +1,14 @@
+﻿// вывод чётных чисел от 1 до N.
+Console.WriteLine("Введите число");
+int n=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Четные числа в интервале между 1 и {n}:");
+if (n>=2)
+{
+ int i=2;
+ while (i<=n)
+  { 
+    Console.Write(i+" ");
+    i=i+2; 
+  }
+}
+else Console.WriteLine("нет положительных четных чисел");
