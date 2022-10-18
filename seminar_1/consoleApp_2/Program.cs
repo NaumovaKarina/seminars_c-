ec0fb1ffd9ba16fc07d5f86fb403fd1b167d4e99
Karina Naumova <karinka_naumova@mail.ru>
2022-10-18 16:02:18 +0300

    добавлена 2 задача

---
+++ seminar_1/consoleApp_2/Program.cs
@@ -0,0 +1,26 @@
+﻿/* на вход принимает два числа 
+выдаёт, какое число большее, а какое меньшее*/
+Console.WriteLine("Введите число А");
+int a=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B");
+int b=Convert.ToInt32(Console.ReadLine());
+if (a >= b)
+{ 
+ if (a > b) 
+ {
+    Console.WriteLine("А-большее число, B-меньшеее число");
+ }  
+ else Console.WriteLine("Числа одинаковые");
+}
+else
+{
+  Console.WriteLine("А-меньшее число, B-большее число");  
+}
+/*int count = 0;
+while(count < 10)
+{
+int what = new Random().Next(0, 3);
+if (what == 0)
+{
+x = (x + xa) / 2;
+y = (y */
---
+++ seminar_1/consoleApp_4/Program.cs
@@ -0,0 +1,11 @@
+﻿// выдаёт максимальное из трех чисел
+Console.WriteLine("Введите число А");
+int a=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B");
+int b=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число С");
+int c=Convert.ToInt32(Console.ReadLine());
+int max=a;
+if (b>max) max=b;
+if (c>max) max=c; 
+Console.WriteLine($"Максимальное число={max}");
