907f4526c1a50899bcb60b8568cf52f5a528b4bd
Karina Naumova <karinka_naumova@mail.ru>
2022-10-18 17:04:04 +0300

    добавлена 6 задача

---
+++ seminar_1/consoleApp_6/Program.cs
@@ -0,0 +1,10 @@
+﻿// проверка числа на четность
+Console.WriteLine("Введите число");
+int a=Convert.ToInt32(Console.ReadLine());
+int max=a;
+if (a%2==0) 
+ {
+    Console.WriteLine($"Число {a} четное");
+ }  
+else
+Console.WriteLine($"Число {a} нечетное");
