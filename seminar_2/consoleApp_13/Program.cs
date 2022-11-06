0d89df7849ebdbfe43b65b114034b67d50f74fe7
Karina Naumova <karinka_naumova@mail.ru>
2022-11-06 23:41:40 +0300

    добавлена 13 задача

---
+++ seminar_2/consoleApp_13/Program.cs
@@ -0,0 +1,11 @@
+﻿/* выводит третью цифру заданного числа
+ или сообщает, что третьей цифры нет */
+Console.WriteLine("Введите число");
+int number=Convert.ToInt32(Console.ReadLine());
+int temp=Math.Abs(number);
+if (temp<100)
+ {Console.WriteLine("третьей цифры нет");};
+while (temp>=1000) 
+ {temp=temp/10;};
+temp=temp%10;  
+Console.WriteLine($"третья цифра - {temp}");
