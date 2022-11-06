3f07859f49cf43fa84d464c945806c72f04cee04
Karina Naumova <karinka_naumova@mail.ru>
2022-11-06 23:12:22 +0300

    добавлена 10 задача

---
+++ seminar_2/consoleApp_10/Program.cs
@@ -0,0 +1,15 @@
+﻿/* принимает на вход трёхзначное число и 
+на выходе показывает вторую цифру этого числа */ 
+Console.WriteLine("Введите трехзначное число");
+int number=Convert.ToInt32(Console.ReadLine());
+int modul=Math.Abs(number);
+while (modul < 100 | modul > 999)
+  {
+   Console.WriteLine("Это не трехзначное число, попробуйте еще раз");
+   Console.WriteLine("Введите трехзначное число");
+   number=Convert.ToInt32(Console.ReadLine());
+   modul=Math.Abs(number);
+  }
+int a1=modul/100;
+int a2=(modul-a1*100)/10;
+Console.WriteLine($"Вторая цифра {a2}");
