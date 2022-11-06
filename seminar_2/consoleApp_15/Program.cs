bb6bc1ab13c936f71378a690f6ec7fb76b3c9764
Karina Naumova <karinka_naumova@mail.ru>
2022-11-07 00:12:35 +0300

    добавлена 15 задача

---
+++ seminar_2/consoleApp_15/Program.cs
@@ -0,0 +1,11 @@
+﻿/* принимает на вход цифру, обозначающую день недели, 
+и проверяет, является ли этот день выходным */
+Console.WriteLine("Введите цифру от 1 до 7");
+int number=Convert.ToInt32(Console.ReadLine());
+if (number>=1 & number<=7)
+ {
+    if (number==6 | number==7)
+         Console.WriteLine("Выходной");
+    else Console.WriteLine("Будни");
+   }
+else Console.WriteLine("Этим числом нельзя обозначить день недели");
