23828bd6f048a532f361adf865c955f9fbf3d35d
Karina Naumova <karinka_naumova@mail.ru>
2022-11-28 20:50:06 +0300

    добавлена 54 задача

--- seminar_8/consoleApp_54/Program.cs
+++ seminar_8/consoleApp_54/Program.cs
@@ -13,11 +13,11 @@ void FillArrayRandom(int[,] array)
   }
 }
 
-int GetMaxItemIndex([,] array,int i);
+int GetMaxItemIndex([,] array,int i, int j1, int j2);
 {
  int index=-1;   
- int max=array[i,1];   
- for (int j=0; j<array.GetLength(1); j++)
+ int max=array[i,j1];   
+ for (int j=j1; j<=j2); j++)
     { 
         if (array[i,j]>max) 
         {
@@ -32,10 +32,14 @@ int GetMaxItemIndex([,] array,int i);
 
 void SortRow([,] array,int i);
 int temp;
-int maxindex=GetMaxItemIndex(array);
+int j1=0;
+int j2=array.GetLength(1);
+
 for (int j=0; j<array.GetLength(1); j++)
     { 
        temp=array[i,j];
+       int maxindex=GetMaxItemIndex(array,i, j, j2);
+
        array[i,j]=array[i,maxindex];
        array[i,maxindex]=temp;
     }
@@ -50,4 +54,16 @@ Console.WriteLine("Введите N:");
 int n=Convert.ToInt32(Console.ReadLine());
 int [,] array=new int[m,n];
 FillArrayRandom(array);
-
+for (int i=0; i<array.GetLength(0); i++)
+  { 
+    SortRow(array,int i);
+  }
+    
+for (int i=0; i<array.GetLength(0); i++)
+  { 
+    for (int j=0; j<array.GetLength(1); j++)
+    { 
+        Console.Write(array[i,j]+" ");
+    }
+  Console.WriteLine();
+  
