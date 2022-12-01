0cc2ec5bdaa755f5c2120eef10912dd64370af8a
Karina Naumova <karinka_naumova@mail.ru>
2022-12-01 17:08:17 +0300

    добавлена 64 задача

---
+++ seminar_9/consoleApp_64/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задайте значение N. Напишите программу, 
+//которая выведет все натуральные числа в промежутке от N до 1. 
+//Выполнить с помощью рекурсии.
+int numberR(int n)
+{ 
+    if (n==1)
+    {
+        return 1;
+    }
+Console.Write(n);
+Console.Write(" ");
+return numberR(n-1);   
+}
+
+
+Console.WriteLine("Введите N:");
+int n=Convert.ToInt32(Console.ReadLine());
+int result=numberR(n);
+Console.WriteLine(result);
+
+
