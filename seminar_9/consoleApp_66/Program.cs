d3c498e75adeec893e71854695578d274960ff59
Karina Naumova <karinka_naumova@mail.ru>
2022-12-01 18:48:27 +0300

    добавлена 66 задача

--- seminar_9/consoleApp_66/Program.cs
+++ seminar_9/consoleApp_66/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задайте значения M и N. Напишите программу, 
+//которая найдёт сумму натуральных элементов в промежутке от M до N.
+int SumInGap(int m, int n)
+{ 
+    if (m==n)
+    {
+        return m;
+    };
+ 
+return SumInGap(m,n-1)+n;   
+}
+Console.WriteLine("Введите M:");
+int m=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите N:");
+int n=Convert.ToInt32(Console.ReadLine());
+if (m>n)
+  { int temp=n;
+   n=m;
+   m=temp;
+  }
+ 
+int result=SumInGap(m,n); 
+Console.WriteLine(result);
